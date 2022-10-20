96def0697176bf10e005e3d861445e57db36f4fc
keamy-eun <eun.wonki014@gmail.com>
2022-10-20 14:57:27 +0900

    sql

---
+++ JOIN/User_Auth_names.sql
@@ -0,0 +1,6 @@
+-- + 4-1: 이름, 이메일, 권한이름 표시. 조건은 이름이 'Teddy','Allen' 
+SELECT users.NAME, users.EMAIL, auth_names.AUTH_NAME
+FROM (users INNER JOIN auths
+ON users.UNIQUE_ID=auths.UNIQUE_ID_USERS
+AND users.NAME in ('Teddy','Allen') ) INNER JOIN auth_names
+									ON auths.UNIQUE_ID_AUTH_NAMES = auth_names.UNIQUE_ID
