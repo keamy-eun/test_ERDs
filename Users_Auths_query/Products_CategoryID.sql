9ad1397fb13765f50f45bde5f4097016743688ec
keamy-eun <eun.wonki014@gmail.com>
2022-10-20 12:40:31 +0900

    sql

--- Users_Auths_query/Products_CategoryID.sql
+++
@@ -1,7 +0,0 @@
--- Products에 CategoryID가 5,6인 Products 이름,공급자 아이
-
-SELECT Products.ProductName, Products이름과 Supplier아이디 출력
-FROM Products
-INNER JOIN Categories
-ON Products.CategoryID=Categories.CategoryID
-AND Categories.CategoryID in (5,6);
